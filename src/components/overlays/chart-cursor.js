b20b2c69785ebde832b8bb7d66bf8c9b21f98868
leoplaw <leo@guildmedia.net>
2022-05-25 22:45:13 +0200

    Cursor - first draft working!

---
+++ src/components/overlays/chart-cursor.js
@@ -0,0 +1,55 @@
+// chart-cursor.js
+
+export default class chartCursor {
+
+  #target
+  #scene
+  #config
+  #chart
+  #xAxis
+  #yAxis
+
+  constructor(target, chart, xAxis, yAxis, config) {
+
+    this.#target = target
+    this.#scene = target.scene
+    this.#config = config
+    this.#chart = chart
+    this.#xAxis = xAxis
+    this.#yAxis = yAxis
+
+    this.#chart.on("mousemove_chart", (e) => { this.draw(e) })
+    // this.#target.viewport.render();
+
+    // let canvas = this.#target.scene.canvas
+    // canvas.addEventListener("mousemove", (e) => this.onMouseMove(e))
+  }
+
+  onMouseMove(e) {
+    this.#chart.emit("mousemove_chart", [e.layerX, e.layerY])
+  }
+
+  draw(e) {
+
+    let [x, y] = e
+
+    this.#scene.clear()
+    const ctx = this.#scene.context
+    ctx.save();
+
+    ctx.strokeStyle = "#666"
+    ctx.beginPath()
+    ctx.moveTo(x, 0)
+    ctx.lineTo(x, this.#scene.height)
+    ctx.stroke()
+
+    ctx.beginPath()
+    ctx.moveTo(0, y)
+    ctx.lineTo(this.#scene.width, y)
+    ctx.stroke()
+
+    ctx.restore();
+
+    this.#target.viewport.render();
+  }
+}
