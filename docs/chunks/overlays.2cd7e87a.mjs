81bd9a78091446099b8a3999d8fc3415ad10e39f
TradeX-app <my.github@tutanota.com>
2023-10-14 13:07:55 +0200

    GitHub Docs

---
+++ docs/chunks/overlays.2cd7e87a.mjs
@@ -0,0 +1,9 @@
+async function getMod() {
+						return import('./overlays.0f5963bb.mjs');
+					}
+					const collectedLinks = "@@ASTRO-LINKS@@";
+					const collectedStyles = "@@ASTRO-STYLES@@";
+					const collectedScripts = "@@ASTRO-SCRIPTS@@";
+					const defaultMod = { __astroPropagation: true, getMod, collectedLinks, collectedStyles, collectedScripts };
+
+export { defaultMod as default };
