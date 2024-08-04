509cac13a198089372cfa78f83f29599d11bbb98
thebadking <53491595+thebadking@users.noreply.github.com>
2024-08-04 21:45:07 +0100

    new nextjs

---
+++ nextjs-tradex/components/tradeX/overlays/hilo.js
@@ -0,0 +1,149 @@
+/* eslint-disable prefer-const */
+/* eslint-disable one-var */
+/* eslint-disable default-param-last */
+// chart-highLow.js
+
+// import Overlay from "./overlay";
+// import { renderLineHorizontal } from "../../renderer/line";
+// import { renderTextBG } from "../../renderer/text";
+import { Overlay, renderLineHorizontal, renderTextBG } from "tradex-chart";
+
+
+const defaults = {
+  colour: "#4444cc88",
+  wdith: 1,
+  dash: [1, 0],
+};
+
+function drawText(ctx, txt, x, y, w, theme) {
+  let options = {
+      fontSize: theme.fontSize * 1.05,
+      fontWeight: theme.fontWeight,
+      fontFamily: theme.fontFamily,
+      txtCol: theme.colourCursor,
+      bakCol: theme.colourCursorBG,
+      paddingTop: 2,
+      paddingBottom: 2,
+      paddingLeft: 3,
+      paddingRight: 3,
+      width: w,
+    },
+    height = options.fontSize + options.paddingTop + options.paddingBottom,
+    yPos = y - height * 0.5;
+
+  ctx.save();
+
+  ctx.fillStyle = options.bakCol;
+  ctx.fillRect(x, yPos, w, height);
+
+  renderTextBG(ctx, `${txt}`, x, yPos, options);
+
+  ctx.restore();
+}
+
+class scaleHighLow extends Overlay {
+  constructor(target, xAxis = false, yAxis = false, theme, parent, params) {
+    super(target, xAxis, yAxis, theme, parent, params);
+
+    this.viewport = target.viewport;
+  }
+
+  // Overlay position is fixed and won't pan / scroll with the chart
+  set position(p) {
+    this.target.setPosition(0, 0);
+  }
+
+  // eslint-disable-next-line class-methods-use-this
+  draw() {}
+
+  scaleDraw(range = this.core.range) {
+    if (!super.mustUpdate()) return;
+
+    this.scene.clear();
+
+    let txt, x, y, w;
+    const hi = range.valueHi;
+    const lo = range.valueLo;
+    const theme = { ...this.theme.yAxis };
+    const ctx = this.scene.context;
+
+    theme.colourCursorBG = this.theme?.hilo?.colour || defaults.colour;
+    // Highest Price
+    txt = this.chart.Scale.nicePrice(hi);
+    x = 1;
+    y = this.yAxis.yPos(hi) + 1;
+    w = this.viewport.width;
+    drawText(ctx, txt, x, y, w, theme);
+
+    // Lowest Price
+    txt = this.chart.Scale.nicePrice(lo);
+    y = this.yAxis.yPos(lo) + 1;
+    drawText(ctx, txt, x, y, w, theme);
+
+    super.updated();
+  }
+}
+
+export default class chartHighLow extends Overlay {
+  constructor(target, xAxis = false, yAxis = false, theme, parent, params) {
+    super(target, xAxis, yAxis, theme, parent, params);
+
+    // add the overlay to the Scale (yAxis)
+    const hiLo = { class: scaleHighLow, fixed: true, required: false };
+    if (this.core.config?.highLow === true)
+      this.scaleOverly = this.chart.scale.addOverlay("hiLo", hiLo);
+  }
+
+  // Overlay position is fixed and won't pan / scroll with the chart
+  set position(p) {
+    this.target.setPosition(0, 0);
+  }
+
+  draw(range = this.core.range) {
+    if (this.core.config?.highLow !== true) return;
+
+    if (!super.mustUpdate()) return;
+
+    this.scene.clear();
+
+    let txt, x, y;
+    let r = this.scene.width;
+    let w = 35;
+    let opts = {};
+    const hi = range.valueHi;
+    const lo = range.valueLo;
+    const theme = { ...this.theme.yAxis };
+    const ctx = this.scene.context;
+
+    theme.colourCursorBG = this.theme?.hilo?.colour || defaults.colour;
+
+    ctx.save();
+    ctx.strokeStyle = this.theme?.highLow?.colour || defaults.colour;
+    ctx.strokeWidth = this.theme?.highLow?.width || defaults.width;
+    ctx.setLineDash(this.theme?.highLow?.dash || defaults.dash);
+
+    // Highest Price
+    y = this.yAxis.yPos(hi);
+    renderLineHorizontal(ctx, y, 0, r, opts);
+
+    txt = "High";
+    x = this.theme.yAxis.location == "left" ? 0 : r - (w + 25);
+    drawText(ctx, txt, x, y, w, theme);
+
+    // Lowest Price
+    y = this.yAxis.yPos(lo);
+    renderLineHorizontal(ctx, y, 0, r, opts);
+
+    txt = "Low";
+    drawText(ctx, txt, x, y, w, theme);
+
+    ctx.restore();
+
+    super.updated();
+
+    if ("hiLo" in this.chart.scale.overlays) {
+      this.chart.scale.overlays.hiLo.instance.setRefresh();
+      this.chart.scale.overlays.hiLo.instance.scaleDraw();
+    }
+  }
+}
