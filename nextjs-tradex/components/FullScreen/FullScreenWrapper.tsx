1724b58d3d891fa1c971e4a982bdf7bcb10518ce
thebadking <53491595+thebadking@users.noreply.github.com>
2024-08-05 01:14:29 +0100

    new nextjs

---
+++ nextjs-tradex/components/FullScreen/FullScreenWrapper.tsx
@@ -0,0 +1,26 @@
+import React, { useEffect, useState } from "react";
+import { FullScreen, useFullScreenHandle } from "react-full-screen";
+
+function iOS() {
+  return (
+    /iPad|iPhone|iPod/.test(navigator.userAgent) ||
+    (navigator.userAgent.includes("Mac") && "ontouchend" in document)
+  );
+}
+
+const FullScreenWrapper = ({ children, ...props }) => {
+  const handle = useFullScreenHandle();
+  const [isIOS, setIsIOS] = useState(false);
+
+  useEffect(() => {
+    setIsIOS(iOS());
+  }, []);
+
+  return (
+    <FullScreen handle={handle} className="full-size">
+      {children({ handle, isIOS, ...props })}
+    </FullScreen>
+  );
+};
+
+export default FullScreenWrapper;
