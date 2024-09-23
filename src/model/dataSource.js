6be791cfe0aa5f32f0dc22a21e6542939e8f68ea
TradeX-app <my.github@tutanota.com>
2024-09-23 23:21:21 +0200

    WIP: DataSource

--- src/model/dataSource.js
+++ src/model/dataSource.js
@@ -11,6 +11,7 @@ import TradeXchart from "../core";
 
 export default class DataSource {
 
+  static #chartList = new xMap()
   static #sourceList= new xMap()
   static #sourceCnt = 0
 
@@ -66,36 +67,115 @@ export default class DataSource {
     return DataSource.#sourceList.get(key)
   }
 
+  /**
+   * List registered data sources
+   * @param {TradeXchart} chart - target
+   * @returns {Array.<DataSource>|undefined} - array of state instances
+   */
+  static list (chart) {
+    let sources = DataSource.chartList(chart)?.sources
+    if (!sources) return undefined
+    
+    return Array.from( sources, 
+      ([key, value]) => ({key, value}))
+  }
+
   #cnt
   #id
+  #symbol
   #state
   #history
-  #stream
-  #data
+  #ticker
   #core
+  #timeFrames = {}
+  #timeFrameActive
+
   #waiting = false
 
-  constructor( state, source, data, core ) {
+
+  /**
+   * Creates an instance of DataSource.
+   * One DataSource per symbol and thus state
+   * @param {Object} cfg - configuration object
+   * @param {String} cfg.symbol - eg. BTC/USDT 
+   * @param {Array.<Number>} cfg.timeFrames - array of time frames in milliseconds
+   * @param {Number} cfg.timeFrameInit - initial time frame
+   * @param {Ticker} cfg.ticker
+   * @param {History} cfg.history
+   * @param {State} cfg.state
+   * @param {TradeXchart} cfg.core
+   * @memberof DataSource
+   */
+  constructor( cfg ) {
 
     this.#cnt = ++DataSource.#sourceCnt
-    this.#id = uid(`${SHORTNAME}_dataSource`)
-    this.#state = state
-    this.#core = core
-    this.#history = source.history
-    this.#stream = source.stream
+    this.#symbol = cfg.symbol
+    this.#id = uid(`${SHORTNAME}_dataSource_${cfg.symbol}`)
+    // convert time frames list into object {1m: 60000}
+    this.#timeFrames = cfg.timeFrames
+    this.#timeFrameActive = cfg.timeFrameInit
+    this.#history = cfg.history
+    this.#ticker = cfg.ticker
+    this.#state = cfg.state
+    this.#core = cfg.core
 
     core.on("range_limitPast", this.onRangeLimit, this)
     // core.on("range_limitFuture", (e) => onRangeLimit(e, "future"))
   }
 
   get id() { return this.#id }
+  get symbol() { return this.#symbol }
+  get timeFrame() { return this.#timeFrameActive }
+  get timeFrameMS() { return this.#timeFrameActive }
+  get timeFrameStr() { return }
+  get timeFrames() { return this.#timeFrames }
+
+  useTimeFrame(tf) {
+
+  }
+
+  tickerAdd(t) {
+
+  }
+
+  tickerStart() {
+
+  }
+
+  tickerStop() {
+
+  }
+
+  tickerError() {
+
+  }
+
+  historyAdd(h) {
+
+  }
+
+  historyFetch() {
+
+  }
+
+  historyError() {
+
+  }
+
+  onTick(t) {
+
+  }
+
+  onFetch(f) {
+
+  }
 
   onRangeLimit(e) {
     if (this.#waiting) return
     this.#waiting = true
     try {
       // history call must handle time outs
-      this.#history( this.#core.symbol, this.#core.timeFrame, this.#core.range.timeStart )
+      this.#history( this.#symbol, this.#core.timeFrame, this.#core.range.timeStart )
       .then(d => {
         this.#core.mergeData(d, false, true)
         this.#waiting = false
